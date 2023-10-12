3d516289fd9921af9765e7fd6a275127928a1553
Raihan Arpe <raihanarpe@gmail.com>
2023-10-12 08:11:00 +0700

    kedua

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use App\Http\Requests\StoreCategoryRequest;
+use App\Http\Requests\UpdateCategoryRequest;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        try {
+
+            $category = Category::all();
+
+            $json = response()->json(
+                [
+                    'status' => 200,
+                    'data' => $category
+                ]
+            );
+
+            return $json;
+        } catch (\Throwable $th) {
+
+            $json = response()->json(
+                [
+                    'status' => 505,
+                    'error' => $th->getMessage()
+                ]
+            );
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCategoryRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Category $category)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCategoryRequest $request, Category $category)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Category $category)
+    {
+        //
+    }
+}
